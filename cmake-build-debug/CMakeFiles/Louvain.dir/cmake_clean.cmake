9320435828a5255993381952c66162d77c3fb630
Stach <staho.kazmierowski@gmail.com>
2020-04-19 12:11:16 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Louvain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Louvain.dir/source/main.cpp.o"
+  "Louvain"
+  "Louvain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Louvain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
